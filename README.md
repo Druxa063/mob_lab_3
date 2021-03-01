6aed0c525e4b0dccc2b9948962bbe44340368b38
Druxa063 <andru-163@mail.ru>
2021-03-01 11:59:33 +0400

    added task, result

---
+++ README.md
@@ -0,0 +1,10 @@
+# lab 3
+
+## Задание 
+
+Реализовать бекенд на CUBA Platform для приложения из лабораторной 2
+
+## Результат
+
+![List users](https://github.com/Druxa063/mob_lab_3/images/users.png)
+![List notes](https://github.com/Druxa063/mob_lab_3/images/notes.png)
--- modules/global/src/com/company/moblab3/entity/Note.java
+++ modules/global/src/com/company/moblab3/entity/Note.java
@@ -22,4 +22,56 @@ public class Note extends StandardEntity {
     @ManyToOne
     @JoinColumn(name = "user_name")
     private User user;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public byte[] getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(byte[] attachment) {
+        this.attachment = attachment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
